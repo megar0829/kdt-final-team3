02652a4050dccb6629ff48ab884d3a9a92b229b9
Lightness-of-Being <gje960217@naver.com>
2023-05-26 14:05:55 +0900

    update restart

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,12 +4,11 @@ from accounts.views import AccountCreateView, AccountDetailView, AccountUpdateVi
 
 app_name = 'account'
 urlpatterns = [
-    path('login/',LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    # 로그인 뷰 // 로그인뷰만 템플릿 입력해야함
-    path('logout/',LogoutView.as_view(),name='logout'),
+    path('login/',, name='login'),
+    path('logout/',,name='logout'),
     #
-    path('create/',AccountCreateView.as_view(),name='create'),
-    path('detail/<int:pk>',AccountDetailView.as_view(),name='detail'),
-    path('update/<int:pk>',AccountUpdateView.as_view(),name='update'),
-    path('delete/<int:pk>',AccountDeleteView.as_view(),name='delete'),
+    path('create/',,name='create'),
+    path('detail/<int:pk>',,name='detail'),
+    path('update/<int:pk>',,name='update'),
+    path('delete/<int:pk>',,name='delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,25 +6,3 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 # Create your views here.
 
-class AccountCreateView(CreateView):
-    model = User
-    form_class = UserCreationForm
-    success_url = reverse_lazy('post:index')
-    template_name = 'accounts/login.html'
-class AccountDetailView(DetailView):
-    model = User
-    context_object_name = 'target_user'
-    template_name = 'accounts/detail.html'
-
-class AccountUpdateView(UpdateView):
-    model = User
-    context_object_name = 'target_user'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('account:detail')
-    template_name = 'accounts/update.html'
-
-class AccountDeleteView(DeleteView):
-    model = User
-    context_object_name = 'target_user'
-    # success_url = reverse_lazy('post:index')
-    template_name = 'accounts/delete.html'
