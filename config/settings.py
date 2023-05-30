d2cb7591eba2cfb1228b73941c88b2454b2589f0
megar0829 <megar0829@gmail.com>
2023-05-30 11:37:50 +0900

    Update:Accounts

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'posts',
     'accounts',
-    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
