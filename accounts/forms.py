b2a21ca4784903cd9c5c7f21e8dc9c88bd6d0123
Jo Gyusang <121420833+Jo-Intelligentsia@users.noreply.github.com>
2023-06-02 11:23:44 +0900

    [UPDATE] login,signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -48,7 +48,6 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = ('username','email','password1','password2')
 
-
 class CustomUserChangeForm(UserChangeForm):
     first_name = forms.CharField(
         label=False, label_suffix='',
